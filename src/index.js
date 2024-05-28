f47280928c7704863358ab82f26522cf1c4f4a8f
boram065 <boram4784@gmail.com>
2024-05-28 10:22:30 +0900

    2024-05-28

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import NewsApp from './components/NewsApp'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 최상위 컴포넌트인 NewsApp은 렌더링하도록 설정
+root.render(<NewsApp />)
